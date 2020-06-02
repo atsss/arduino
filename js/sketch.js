07d408b2640152770bf4c4044b831d2fd9635318
atsss <atsushi0623itoh@gmail.com>
2020-06-02 16:58:46 -0600

    create p5.js files

---
+++ js/sketch.js
@@ -0,0 +1,33 @@
+//  an instance of the serialport library:
+let serial;
+// Change this to the name of your serial port here:
+const portName = '/dev/cu.usbmodem14201';
+
+function setup() {
+  createCanvas(500, 600, WEBGL);
+  // instantiate serial port:
+  serial = new p5.SerialPort();
+  // set callback for port open:
+  serial.on('open', startSerial);
+  // set callback for incoming data:
+  serial.on('data', serialEvent);
+  // open serial port:
+  serial.open(portName);
+}
+
+// callback function for when serial port opens:
+function startSerial() {
+  // send an initial byte
+  // to get data from the microcontroller:
+  serial.write('x');
+  console.log('serial port opened');
+}
+
+// callback function for incoming serial data:
+function serialEvent() {
+  // read from port until new line:
+  let message = serial.readStringUntil('\n');
+  if (message != null) {
+  console.log(message);
+  }
+}
