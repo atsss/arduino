78e0bf2269968c8718ed7fa24f29193a567ab6fd
atsss <atsushi0623itoh@gmail.com>
2020-06-02 16:25:36 -0600

    add Madgwick library

--- p5js.cpp
+++ p5js.cpp
@@ -1,7 +1,22 @@
 #include <Arduino_LSM6DS3.h>
+#include <MadgwickAHRS.h>
+
+// initialize a Madgwick filter:
+Madgwick filter;
+// sensor's sample rate is fixed at 104 Hz:
+const float sensorRate = 104.00;
+
+// values for orientation:
+float roll = 0.0;
+float pitch = 0.0;
+float heading = 0.0;
 
 void setup() {
   Serial.begin(9600);
+
+  // start the filter to run at the sample rate:
+  filter.begin(sensorRate);
+
   // attempt to start the IMU:
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU");
@@ -21,17 +36,19 @@ void loop() {
     IMU.readAcceleration(xAcc, yAcc, zAcc);
     IMU.readGyroscope(xGyro, yGyro, zGyro);
 
-    Serial.print("sensors: ");
-    Serial.print(xAcc);
+    // update the filter, which computes orientation:
+    filter.updateIMU(xGyro, yGyro, zGyro, xAcc, yAcc, zAcc);
+
+    // print the heading, pitch and roll
+    roll = filter.getRoll();
+    pitch = filter.getPitch();
+    heading = filter.getYaw();
+
+    // print the filter's results:
+    Serial.print(heading);
     Serial.print(",");
-    Serial.print(yAcc);
+    Serial.print(pitch);
     Serial.print(",");
-    Serial.print(zAcc);
-    Serial.print(",");
-    Serial.print(xGyro);
-    Serial.print(",");
-    Serial.print(yGyro);
-    Serial.print(",");
-    Serial.println(zGyro);
+    Serial.println(roll);
   }
 }
